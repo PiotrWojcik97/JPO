31b1e65c2205c78181702c8bc423b81adb1d7c44
Piotr <p.p.wojcik@interia.pl>
2018-03-29 23:51:15 +0200

    pktMat

---
+++ java/01/pktMat.java
@@ -0,0 +1,49 @@
+public class pktMat 
+{
+	private int masa;				//masa
+	
+	public pktMat()
+	{ 								//konstruktor domyślny
+	this.masa = 1;
+	}
+
+	public pktMat(int a){			//konstruktor z parametrem
+		if(a>0)
+		{
+			this.masa=a;
+		}
+		else{
+			System.out.println("Masa musi byc wieksza od zera");
+		}
+	}
+
+	public int getMasa(){			//akcesor
+		return this.masa;
+	}
+
+	public int zmienMase(int c)		//akcesor
+	{
+		this.masa = c;
+		return this.masa;
+	}
+
+	public int momBez()
+	{ 								//klasa obliczająca moment bezwładności
+		int i = 0;
+		return i;
+	}
+
+	public int steiner(int d)
+	{ 								//klasa obliczająca moment bezwładości z twierdzenia Steinera
+		int is;
+		is = momBez() + this.masa*d*d;
+		return is;
+	}
+
+	public void opis()
+	{ 								//klasa zwracająca opis
+		System.out.println("Punkt materialny");
+	}
+	
+}
+
